1e6b88d1d6e7ea5bca60c342b0f1faa5a425c984
Artem Boyarskov <arboyarskov@ya.ru>
2022-09-19 01:07:15 +0300

    Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkParse() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("is empty");
+    }
+
+    @Test
+    void validateNotSymbol() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "name";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(name)
+                .hasMessageContaining("does not contain the symbol");
+    }
+
+    @Test
+    void validateNotKey() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "=name";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(name)
+                .hasMessageContaining("does not contain a key");
+    }
+
+    @Test
+    void validateNotValue() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "name=";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("^.+")
+                .hasMessageContaining(name)
+                .hasMessageContaining("does not contain a value");
+    }
+}
