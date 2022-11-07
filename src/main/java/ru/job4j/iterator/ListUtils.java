123dafc373d03370bd75f75f03bd773ff8307367
Artem Boyarskov <arboyarskov@ya.ru>
2022-11-07 15:17:31 +0300

    7. ListIterator [#350217]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -5,7 +5,6 @@ import java.util.function.Predicate;
 
 public class ListUtils {
     public static <T> void addBefore(List<T> list, int index, T value) {
-        //метод который будет вставлять значение перед индексом
         Objects.checkIndex(index, list.size());
         ListIterator<T> iterator = list.listIterator();
         while (iterator.hasNext()) {
@@ -18,30 +17,41 @@ public class ListUtils {
     }
 
     public static <T> void addAfter(List<T> list, int index, T value) {
-        //addAfter() вставляет после индекса;
-
-    }
-
-    public static <T> void addBefore(List<T> list, int index, int x) {
-        //addBefore() вставляет до индекса
-
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.previousIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
     }
 
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
-        //removeIf() удаляет все элементы, которые удовлетворяют предикату.
-        // Запрещено использовать метод List.removeIf;
-
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
+            }
+        }
     }
 
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
-        //replaceIf() заменяет все элементы, которые удовлетворяют предикату;
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.set(value);
+            }
+        }
     }
 
     public static <T> void removeAll(List<T> list, List<T> elements) {
-        //removeAll() удаляет из списка те элементы, которые есть в elements
-        //Запрещено использовать метод List.removeAll().
-
+        ListIterator<T> iterator = list.listIterator();
+        for (T element : elements) {
+            while (iterator.next().equals(element)) {
+                iterator.remove();
+            }
+        }
     }
-
-
 }
--- src/test/java/ru/job4j/iterator/ListUtilsTest.java
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -1,24 +1,37 @@
 package ru.job4j.iterator;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.*;
 
+import static org.assertj.core.api.Assertions.*;
+
 class ListUtilsTest {
 
     private List<Integer> input;
+    private List<Integer> input2;
 
     @BeforeEach
     void setUp() {
         input = new ArrayList<>(Arrays.asList(1, 3));
+        input2 = new ArrayList<>(Arrays.asList(4, 2, 3, 5, 2, 1));
+    }
+
+    @FunctionalInterface
+    interface Predicate<T> {
+        boolean test(T t);
     }
 
     @Test
     void whenAddBefore() {
         ListUtils.addBefore(input, 1, 2);
         assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAddBefore2() {
+        ListUtils.addBefore(input2, 3, 9);
+        assertThat(input2).hasSize(7).containsSequence(4, 2, 3, 9, 5, 2, 1);
     }
 
     @Test
@@ -32,5 +45,30 @@ class ListUtilsTest {
         ListUtils.addAfter(input, 0, 2);
         assertThat(input).hasSize(3).containsSequence(1, 2, 3);
     }
+
+    @Test
+    void whenAddAfterWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addAfter(input, 3, 2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenRemoveIf() {
+        ListUtils.removeIf(input2, x -> x < 3);
+        assertThat(input2).hasSize(3).containsSequence(4, 3, 5);
+    }
+
+    @Test
+    void whenReplaceIf() {
+        ListUtils.replaceIf(input2, x -> x > 4, 77);
+        assertThat(input2).hasSize(6).containsSequence(4, 2, 3, 77, 2, 1);
+    }
+
+    @Test
+    void whenRemoveAll() {
+        ListUtils.removeAll(input, new ArrayList<>(Arrays.asList(1, 3)));
+        assertThat(input).hasSize(0);
+    }
+
 
 }
